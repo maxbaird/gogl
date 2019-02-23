b648fc89c05c149be63245ce168359c433b6f04f
Max Baird <mmb1@hw.ac.uk>
2019-02-23 22:53:47 +0000

    Added function to create program

--- gogl.go
+++ gogl.go
@@ -5,13 +5,24 @@ import (
 	"strings"
 )
 
+type (
+	//ShaderID ...
+	ShaderID uint32
+	//ProgramID ...
+	ProgramID uint32
+	//VAOID ...
+	VAOID uint32
+	//VBOID ...
+	VBOID uint32
+)
+
 //GetVersion ...
 func GetVersion() string {
 	return gl.GoStr(gl.GetString(gl.VERSION))
 }
 
-//MakeShader ...
-func MakeShader(shaderSource string, shaderType uint32) uint32 {
+//CreateShader ...
+func CreateShader(shaderSource string, shaderType uint32) ShaderID {
 	shaderID := gl.CreateShader(shaderType)
 	shaderSource = shaderSource + "\x00"
 	csource, free := gl.Strs(shaderSource)
@@ -30,5 +41,28 @@ func MakeShader(shaderSource string, shaderType uint32) uint32 {
 		gl.GetShaderInfoLog(shaderID, logLength, nil, gl.Str(log))
 		panic("Failed to compile shader: \n" + log)
 	}
-	return shaderID
+	return ShaderID(shaderID)
+}
+
+//CreateProgram ...
+func CreateProgram(vert ShaderID, frag ShaderID) ProgramID {
+	shaderProgram := gl.CreateProgram()
+	gl.AttachShader(shaderProgram, uint32(vert))
+	gl.AttachShader(shaderProgram, uint32(frag))
+	gl.LinkProgram(shaderProgram)
+
+	var success int32
+	gl.GetProgramiv(shaderProgram, gl.LINK_STATUS, &success)
+
+	if success == gl.FALSE {
+		var logLength int32
+		gl.GetProgramiv(shaderProgram, gl.INFO_LOG_LENGTH, &logLength)
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetProgramInfoLog(shaderProgram, logLength, nil, gl.Str(log))
+		panic("Failed to link program: \n" + log)
+	}
+	gl.DeleteShader(uint32(vert))
+	gl.DeleteShader(uint32(frag))
+
+	return ProgramID(shaderProgram)
 }
