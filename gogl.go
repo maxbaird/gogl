dffebe3876cf566d3a8474abda075041fb50d212
Max Baird <mmb1@hw.ac.uk>
2019-02-23 22:28:17 +0000

    Added function to make shader

--- gogl.go
+++ gogl.go
@@ -2,9 +2,31 @@ package gogl
 
 import (
 	"github.com/go-gl/gl/v3.3-core/gl"
+	"strings"
 )
 
 //GetVersion ...
 func GetVersion() string {
 	return gl.GoStr(gl.GetString(gl.VERSION))
+}
+
+//MakeShader ...
+func MakeShader(shaderSource string, shaderType uint32) uint32 {
+	shaderID := gl.CreateShader(shaderType)
+	shaderSource = shaderSource + "\x00"
+	csource, free = gl.Strs(shaderSource)
+	gl.ShaderSource(shaderID, 1, csource, nil)
+	free()
+
+	gl.CompileShader(shaderID)
+	gl.GetShaderiv(shaderID, gl.COMPILE_STATUS, &status)
+
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shaderID, gl.INFO_LOG_LENGTH, &logLength)
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(shaderID, logLength, nil, gl.Str(log))
+		panic("Failed to compile shader: \n" + log)
+	}
+	return shaderID
 }
