a784bdfef293d119768ab97c8fbd5c74cd0ab4d0
Max Baird <mmb1@hw.ac.uk>
2019-02-24 12:23:32 +0000

    Still working on hotloading shaders

--- gogl.go
+++ gogl.go
@@ -2,10 +2,10 @@ package gogl
 
 import (
 	"errors"
-	"fmt"
+	//"fmt"
 	"github.com/go-gl/gl/v3.3-core/gl"
 	"io/ioutil"
-	"os"
+	//"os"
 	"strings"
 	"time"
 )
@@ -21,8 +21,9 @@ type (
 	VBOID uint32
 )
 
-type shaderInfo struct {
-	path     string
+type programInfo struct {
+	vertPath string
+	fragPath string
 	modified time.Time
 }
 
@@ -31,23 +32,25 @@ func GetVersion() string {
 	return gl.GoStr(gl.GetString(gl.VERSION))
 }
 
-var loadedShaders []shaderInfo
+var loadedShaders []programInfo
 
 //CheckShadersForChanges ...
 func CheckShadersForChanges() {
-	for _, shaderInfo := range loadedShaders {
-		file, err := os.Stat(shaderInfo.path)
+	/*
+		for _, shaderInfo := range loadedShaders {
+			file, err := os.Stat(shaderInfo.path)
 
-		if err != nil {
-			panic(err)
+			if err != nil {
+				panic(err)
+			}
+
+			modTime := file.ModTime()
+
+			if !modTime.Equal(shaderInfo.modified) {
+				fmt.Println("Shader modified")
+			}
 		}
-
-		modTime := file.ModTime()
-
-		if !modTime.Equal(shaderInfo.modified) {
-			fmt.Println("Shader modified")
-		}
-	}
+	*/
 }
 
 //LoadShader ...
@@ -64,16 +67,6 @@ func LoadShader(path string, shaderType uint32) (ShaderID, error) {
 	if err != nil {
 		return 0, err
 	}
-
-	file, err := os.Stat(path)
-
-	if err != nil {
-		panic(err)
-	}
-
-	modTime := file.ModTime()
-	loadedShaders = append(loadedShaders, shaderInfo{path, modTime})
-
 	return shaderID, nil
 }
 
@@ -130,6 +123,18 @@ func CreateProgram(vertPath string, fragPath string) (ProgramID, error) {
 	}
 	gl.DeleteShader(uint32(vert))
 	gl.DeleteShader(uint32(frag))
+
+	//TODO finish hotloading shaders
+	/*
+		file, err := os.Stat(path)
+
+		if err != nil {
+			panic(err)
+		}
+
+		modTime := file.ModTime()
+		loadedShaders = append(loadedShaders, shaderInfo{path, modTime})
+	*/
 
 	return ProgramID(shaderProgram), nil
 }
